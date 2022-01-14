e91b3ea941d8771be5cc1f621dc66c10a9bf76c4
Rio Ananta <rioananta1@gmail.com>
2022-01-14 16:39:39 +0700

    Adding sections

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './layout/Header'
 import Banner from './layout/Banner'
+import Divider from './layout/Divider'
+import Enjoy from './layout/Sections/enjoy.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
+      <Divider />
+      <Enjoy />
+      <Divider />
     </div>
   );
 }
